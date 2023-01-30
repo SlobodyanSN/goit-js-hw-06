10861d20d5286668c48a44822ed6c9e4a5cdd61d
SlobodyanSN <110773219+SlobodyanSN@users.noreply.github.com>
2023-01-30 21:52:58 +0200

    Update task-01.js
    1-st_task

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@
 
 // // Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
 
-// // Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст 
+// // Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст
 // заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
 // // В результаті, в консолі будуть виведені наступні повідомлення.
 
@@ -17,26 +17,19 @@
 // // Category: Technologies
 // // Elements: 5
 
+const categories = document.querySelectorAll(`#categories .item`);
 
+console.log('Number of categories:' + categories.length);
 
-console.log('Number of categories:' + document.querySelectorAll(`#categories .item`).length);
+console.log("");
 
-console.log(`Category: `+ document.querySelector(`ul li h2`).textContent);
+categories.forEach( category => {
 
+    console.log(`Category: ` + category.firstElementChild.textContent);
+ 
+    console.log(`Elements: ` + category.querySelectorAll(`ul li`).length);
 
+    console.log(``);
+});
 
-console.log(`Elements: `+document.querySelectorAll(` #categories .item > ul > li`).length )
-
-// const category = document.querySelectorAll(`#categories .item `);
-
-// const catName = document.querySelector(`#categories .item`);
-
-// console.log(catName)
-
-// console.log(document.querySelectorAll(`#categories .item `))
-
-
-// document.querySelectorAll(`#categories .item`).forEach(element => {
-    
-// });
 
